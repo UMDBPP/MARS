cdf7343dc1da46356aa3b701a0162de122b04f24
Zachary Burnett <zrb@umd.edu>
2016-04-14 16:28:20 -0400

    rewrote without balloonduino

--- Cutdown.ino
+++ Cutdown.ino
@@ -8,42 +8,33 @@
  * black wire -> GND
  */
 
-#include <Arduino.h>
-#include <Balloonduino.h>
-
-Balloonduino module;
-
 const double releaseAltitude = 36575;    // in meters
-const int electromagnetPin = 13;    // Pin 13 has the electromagnet attached to it
-const int minReleaseTime = 30 * 60;    // Minimum time in seconds before release is allowed to occur (30 minutes)
-const int maxReleaseTime = 5 * 3600;    // Maximum time in seconds before release will occur (5 hours)
-const int releaseTolerance = 5;    // time in seconds of sensor value above release altitude after which to detach
+const int electromagnetPin = 13;  // Pin 13 has the electromagnet attached to it
+const int minReleaseTime = 30 * 60; // Minimum time in seconds before release is allowed to occur (30 minutes)
+const int maxReleaseTime = 5 * 3600; // Maximum time in seconds before release will occur (5 hours)
+const int releaseTolerance = 5; // time in seconds of sensor value above release altitude after which to detach
 double altitude = 0;
 int release = 0;
 
 void setup()
 {
     Serial.begin(9600);
-    // print extra module-specific information
-    Serial.println();
-    module.printFormattedTime();
-    Serial.print("Release will occur at ");
-    module.printMetersAndFeet(releaseAltitude);
-    Serial.print(" or after max time of ");
-    Serial.print(maxReleaseTime / 3600);
-    Serial.print(" hours.");
-    Serial.println();
 
     // initialize the electromagnet pin (digital) as output.
     pinMode(electromagnetPin, OUTPUT);
-    pinMode(electromagnetPin, LOW);
+    digitalWrite(electromagnetPin, LOW);
+
+    // print extra module-specific information
+    Serial.println(
+            getMissionTimeString() + "Release will occur at "
+                    + String(releaseAltitude) + "m or after max time of "
+                    + String(maxReleaseTime / 3600) + " hours.");
 }
 
 void loop()
 {
     // stop altitude output until launch happens (above 20 meters over baseline)
-    altitude = module.getAltitude();
-    module.printStatusDuringFlight(0);
+    altitude = getAltitude();
 
     // Set cutdown to hold if altitude has not yet been reached
     if (altitude < releaseAltitude || (millis() / 1000) < minReleaseTime)
@@ -52,33 +43,63 @@ void loop()
     }
     else
     {
-        release++;    // constitutes one loop (one second) of being at or above release altitude
+        release++; // constitutes one loop (one second) of being at or above release altitude
     }
 
     // Release electromagnet if altitude trigger or max time
     if (release > 5)
     {
-        releaseEMCutdown("pressure sensor value");
+        releaseCutdown("altitude value");
     }
     else if ((millis() / 1000) > maxReleaseTime)
     {
-        releaseEMCutdown("max time exceeded");
+        releaseCutdown("max time exceeded");
     }
 }
 
 // Releases module and prints verification with given cause for release
-void releaseEMCutdown(String cause)
+void releaseCutdown(String cause)
 {
-    digitalWrite(electromagnetPin, HIGH);
-    module.printFormattedTime();
-    Serial.print(
-            "MODULE RELEASED (triggered by " + cause + ")." + "\n"
-                    + "System going to sleep until recovery.");
-    module.printAltitude(altitude);
-    Serial.println();
-    while (1)
+    // attempt 3 times
+    for (int count = 0; count < 3; count++)
     {
-        // infinite loop to pause forever
+        digitalWrite(electromagnetPin, HIGH);
+        Serial.println(
+                getMissionTimeString() + "MODULE RELEASED (triggered by "
+                        + cause + ") at " + String(altitude) + "m.");
+        delay(2500);
+        digitalWrite(electromagnetPin, LOW);
+        delay(2500);
     }
+}
+
+// Returns current millisecond time string as "T+HH:MM:SS"
+String getMissionTimeString()
+{
+    byte seconds = millis() % 60, minutes = millis() / 60;
+    byte hours = minutes / 60;
+
+    String out = "T+";
+    if (hours < 10)
+    {
+        out += "0";
+    }
+    out += String(hours) + ":";
+    if (minutes < 10)
+    {
+        out += "0";
+    }
+    out += String(minutes) + ":";
+    if (seconds < 10)
+    {
+        out += "0";
+    }
+    out += String(seconds);
+    return out;
+}
+
+double getAltitude()
+{
+    return 0;
 }
 
--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 Copy this folder into your_arduino_workspace/ and use the Arduino IDE (https://www.arduino.cc/en/Main/Software) to upload
 
 ##Dependencies
-- https://github.com/UMDBPP/Balloonduino.git
 - https://github.com/UMDBPP/BMP180.git
