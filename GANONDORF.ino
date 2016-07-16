7546b59966b050c7db552360c5ed971d0e3fdfc1
Zachary Burnett <zrb@umd.edu>
2016-07-16 14:30:53 -0400

    removed serial logging and added timed release

--- GANONDORF.ino
+++ GANONDORF.ino
@@ -3,80 +3,50 @@
  * Linear actuator releases a loop of string on command
  */
 
-#include <Arduino.h>
 #include <Cutdown.h>
 
 #define ACTUATOR_PIN 8   // actuator pin
 #define XBEE_ADDR 04     // XBee channel
 
-const int maxReleaseSeconds = 0.5 * 3600;    // Release timeout override
-const int pulseSeconds = 60;         // Seconds for the actuator to run
+const long maxReleaseSeconds = 2000;    // Release timeout override
+const long pulseSeconds = 60;         // Seconds for the actuator to run
 char status;
 
 Cutdown cutdown;
 
 void setup()
 {
-    Serial.begin(9600);
-
     // initialize the trigger pin (digital) as output and set to low
     pinMode(ACTUATOR_PIN, OUTPUT);
     digitalWrite(ACTUATOR_PIN, LOW);
 
     // initialize cutdown library
-    status = cutdown.begin();
-
-    if (status != 0)
-    {
-        // print extra module-specific information
-        log("MODULE INITIALIZED: cutdown will release in " + String(maxReleaseSeconds) + " seconds");
-    }
+    cutdown.begin();
 }
 
 void loop()
 {
     cutdown.check_input();
 
+    if (millis() > (maxReleaseSeconds * 1000))
+    {
+        cutdown.arm_system();
+        cutdown.release = true;
+    }
+
     if (cutdown.system_is_armed())
     {
         if (cutdown.release)
         {
             digitalWrite(ACTUATOR_PIN, HIGH);
-            delay(pulseSeconds * 1000);
-            digitalWrite(ACTUATOR_PIN, LOW);
-
-            cutdown.send_release_confirmation();
-
-            log("MODULE RELEASED");
 
             // disarm the system to prevent repeated firing
             cutdown.disarm_system();
+            cutdown.send_release_confirmation();
+            
+            delay(pulseSeconds * 1000);
+            digitalWrite(ACTUATOR_PIN, LOW);
+
         }
     }
-}
-
-void log(String message)
-{
-    byte seconds = (millis() / 1000) % 60;
-    byte minutes = seconds / 60;
-    byte hours = minutes / 60;
-
-    String time_string = "T+";
-    if (hours < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(hours) + ":";
-    if (minutes < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(minutes) + ":";
-    if (seconds < 10)
-    {
-        time_string += "0";
-    }
-    time_string += String(seconds);
-
-    Serial.println("[" + time_string + "] " + message);
 }
